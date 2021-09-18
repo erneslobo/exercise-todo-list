455aec976e35dfb1731b239945d5a9baf9b29c82
erneslobo <erneslobo@gmail.com>
2021-09-18 01:15:38 +0000

    tasklist added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+	const [count, setCount] = useState(0);
+	const [show, setShow] = useState(false);
+
+	let addtask = () => {
+		setTasks([...tasks, task]);
+		setCount(count + 1);
+		setTask("");
+	};
+
+	let deleteTask = value => {
+		setTasks(tasks.filter((task, index) => index !== value));
+		setCount(count - 1);
+	};
+
+	const listTasks = tasks.map((task, index) => (
+		<li
+			className="list-group-item"
+			key={index}
+			onMouseEnter={() => setShow(true)}
+			onMouseLeave={() => setShow(false)}>
+			{task}
+			{show && (
+				<i
+					className="fas fa-times float-end"
+					onClick={() => deleteTask(index)}></i>
+			)}
+		</li>
+	));
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5">
+			<h1 className="text-center">todos</h1>
+			<input
+				className="form-control"
+				type="text"
+				placeholder="What needs to be done?"
+				value={task}
+				onChange={e => setTask(e.target.value)}
+				onKeyPress={e =>
+					e.key === "Enter" && addtask(e.target.value)
+				}></input>
+			<ul className="list-group">
+				{listTasks}
+				<li className="list-group-item">{`${count} item left`}</li>
+			</ul>
 		</div>
 	);
 };
